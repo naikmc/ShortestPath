ac2440319ab1b6da8a1e343e5bcdb181b5fe0da0
App Automation01 <App Automation01>
2016-12-22 22:52:05 -0500

    Initial commit

---
+++ app/src/androidTest/java/shortestpath/naik/mahesh/shortestpath/ExampleInstrumentedTest.java
@@ -0,0 +1,69 @@
+package shortestpath.naik.mahesh.shortestpath;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+
+    int [] pathlements ={0,3,0,1};
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Before
+    public void initValidString() {
+        // Specify a valid string.
+    }
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("shortestpath.naik.mahesh.shortestpath", appContext.getPackageName());
+    }
+
+    @Test
+    public void checkPathpossible() {
+        onView(withId(R.id.pathpossible))
+                .check(matches(withText("Yes")));
+    }
+
+
+    @Test
+    public void checkTotalcost() {
+        onView(withId(R.id.pathsum))
+                .check(matches(withText("27")));
+    }
+
+    @Test
+    public void checkPathElements() {
+        onView(withId(R.id.pathlements))
+                .check(matches(withText(Arrays.toString(pathlements))));
+    }
+}
